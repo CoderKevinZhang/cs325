5e2490a2a96bfd0d0158ca96bf0724a9d63edc25
kenoteb <brykenote@gmail.com>
2017-02-09 22:27:02 -0800

    create distance table give two strings

--- assignment2/align.py
+++ assignment2/align.py
@@ -1,3 +1,4 @@
+import pdb
 from sys import argv
 
 def getCost(old, new):
@@ -16,23 +17,42 @@ def getCost(old, new):
 
     return int(costMatrix[row][col]);
 
+def editDistance(pair):
+    editTable = []
+    for i in range(len(pair[1]) + 1):
+        #pdb.set_trace()
+        tableRow = []
+        for j in range(len(pair[0]) + 1):
+            if i == 0:
+                tableRow.append(j)
+            elif j == 0:
+                tableRow.append(i)
+            elif pair[1][i-1] != pair[0][j-1]:
+                cell = 1 + min(editTable[i-1][j-1], editTable[i-1][j], tableRow[j-1])
+                tableRow.append(cell)
+            else:
+                cell = editTable[i-1][j-1]
+                tableRow.append(cell)
+
+        editTable.append(tableRow)
+
+    return editTable
 
 script, costName, inputName, outputName = argv
 
 costMatrix = []
-pairs = []
 
 with open(costName, 'r') as f:
     for line in f:
         costMatrix.append(line.split(','))
 
+inputPairs = []
+
 with open(inputName, 'r') as f:
     for line in f:
         pair = line.split(',')
-        pairs.append(pair)
+        inputPairs.append(pair)
 
-# INSERT ALGORITHM HERE #
-
-with open(outputName, 'w') as f:
-    for pair in pairs:
-        f.write(",".join(pair))
+#with open(outputName, 'w') as f:
+#    for pair in inputPairs:
+#        f.write(",".join(editDistance(pair)))
