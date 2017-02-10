dfdc4aaebcce560fe5ef6454479ae414a1fd670f
kenoteb <brykenote@gmail.com>
2017-02-09 18:26:31 -0800

    read cost matrix as 2d array

--- assignment2/align.py
+++ assignment2/align.py
@@ -1,13 +1,20 @@
 from sys import argv
 
-script, costMatrix, inputName, outputName = argv
+script, costName, inputName, outputName = argv
 
+costMatrix = []
 pairs = []
+
+with open(costName, 'r') as f:
+    for line in f:
+        costMatrix.append(line.split(','))
 
 with open(inputName, 'r') as f:
     for line in f:
         pair = line.split(',')
         pairs.append(pair)
+
+# INSERT ALGORITHM HERE #
 
 with open(outputName, 'w') as f:
     for pair in pairs:
