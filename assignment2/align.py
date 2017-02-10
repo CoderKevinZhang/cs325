08ad288b3b9309004a1707734a675479feafcba6
kenoteb <brykenote@gmail.com>
2017-02-10 12:49:23 -0800

    simple refactor

--- assignment2/align.py
+++ assignment2/align.py
@@ -18,24 +18,27 @@ def getCost(old, new):
     return int(costMatrix[row][col]);
 
 def createTable(pair):
+    oldString = pair[0]
+    newString = pair[1]
+
     editTable = []
-    for i in range(len(pair[1]) + 1):
+    for i in range(len(newString) + 1):
         tableRow = []
-        for j in range(len(pair[0]) + 1):
+        for j in range(len(oldString) + 1):
             if i == 0 and j == 0:
                 tableRow.append(getCost('-','-'))
             elif i == 0:
                 #tableRow.append(j)
-                tableRow.append(getCost('-',pair[0][j-1]))
+                tableRow.append(getCost('-',oldString[j-1]))
             elif j == 0:
                 #tableRow.append(i)
-                tableRow.append(getCost(pair[1][i-1], '-'))
-            elif pair[1][i-1] != pair[0][j-1]:
+                tableRow.append(getCost(newString[i-1], '-'))
+            elif newString[i-1] != oldString[j-1]:
                 #cell = 1 + min(editTable[i-1][j-1], editTable[i-1][j], tableRow[j-1])
-                cell = getCost(pair[0][j-1], pair[1][i-1]) + min(editTable[i-1][j-1], editTable[i-1][j], tableRow[j-1])
+                cell = getCost(oldString[j-1], newString[i-1]) + min(editTable[i-1][j-1], editTable[i-1][j], tableRow[j-1])
                 tableRow.append(cell)
             else:
-                cell = getCost(pair[0][j-1], pair[1][i-1]) + editTable[i-1][j-1]
+                cell = getCost(oldString[j-1], newString[i-1]) + editTable[i-1][j-1]
                 tableRow.append(cell)
 
         editTable.append(tableRow)
@@ -43,6 +46,9 @@ def createTable(pair):
     return editTable
 
 def findPath(pair, editTable):
+    oldString = pair[0]
+    newString = pair[1]
+
     s1 = ""
     s2 = ""
 
@@ -60,36 +66,36 @@ def findPath(pair, editTable):
 
             '''
             if topLeft == minCell and editTable[y][x] > minCell: # edit (diag)
-                s1 = pair[0][x-1] + s1
-                s2 = pair[1][y-1] + s2
+                s1 = oldString[x-1] + s1
+                s2 = newString[y-1] + s2
                 y -= 1
                 x -= 1
 
             elif topLeft == minCell: # do nothing (diag)
             '''
             if topLeft == minCell: # edit or do nothing (diag)
-                s1 = pair[0][x-1] + s1
-                s2 = pair[1][y-1] + s2
+                s1 = oldString[x-1] + s1
+                s2 = newString[y-1] + s2
                 y -= 1
                 x -= 1
 
             elif top == minCell: # insert (up)
                 s1 = "-" + s1
-                s2 = pair[1][y-1] + s2
+                s2 = newString[y-1] + s2
                 y -= 1
 
             elif left == minCell: # delete (left)
-                s1 = pair[0][x-1] + s1
+                s1 = oldString[x-1] + s1
                 s2 = "-" + s2
                 x -= 1
 
         elif y > 0: # insert the rest of s2
             s1 = "-" + s1
-            s2 = pair[1][y-1] + s2
+            s2 = newString[y-1] + s2
             y -= 1
 
         elif x > 0: # delete the remaining s1
-            s1 = pair[0][x-1] + s1
+            s1 = oldString[x-1] + s1
             s2 = "-" + s2
             x -= 1
 
